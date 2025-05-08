ffa98148e6464026d272e2f8da5b6ca139dc3897
IdaraNnamso <excellencennamso@gmail.com>
2025-05-08 16:04:03 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 1234, // You can change this if you like, Vite's default is 5173
+  },
+})
